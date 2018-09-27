b7d403ea24a53e89870eafb353563ac5de0535ef
Veerakumar <veerakumarcse24@gmail.com>
2018-09-27 09:21:18 +0530

    for create first view

--- backend/ai_chatbot/ai_chatbot/urls.py
+++ backend/ai_chatbot/ai_chatbot/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from ai_chatbot.views.views import hello
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', hello),
 ]
